df6b14afb703003806a8486816f94f6a05b0d124
Rodrigo de Araujo Leite Pereira <rodrigoaraujo1709@gmail.com>
2022-01-16 16:17:03 -0300

    userParams

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -26,7 +26,7 @@ function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
             .then(response => {
                 const data = response.data as MoviePage;
                 console.log(data);
@@ -47,11 +47,6 @@ function Listing() {
                         </div>
                     )
                     )}
-
-
-
-
-
                 </div>
             </div>
         </>
